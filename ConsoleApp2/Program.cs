e7333a14813f9160a636559659315ca0415e3062
HOME <HOME@DESKTOP-SSHKREF>
2023-01-09 15:55:31 +0100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the base of the triangle: ");
+            double baza = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the height of the triangle: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double area = (baza * height) / 2;
+            Console.WriteLine("The area of the triangle is: " + area);
+        }
+    }
+}
